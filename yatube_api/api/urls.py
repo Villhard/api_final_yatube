b0ce589c06fe9b899ac379fe0fae463c369db03c
Victor Ryabov <victorwillhard@gmail.com>
2024-04-29 09:16:42 +0300

    Add CommentViewSet for managing comments on posts

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -8,6 +8,5 @@ router_v1.register(r"posts", PostViewSet)
 
 urlpatterns = [
     path("v1/", include(router_v1.urls)),
-    path("v1/", include("djoser.urls")),
     path("v1/", include("djoser.urls.jwt"))
 ]
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,21 +1,40 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import viewsets, status
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from posts.models import Post
-from api.serializers import PostSerializer
+from api.serializers import PostSerializer, CommentSerializer
 from api.permissions import IsAuthorOrReadOnly
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = (IsAuthenticated, IsAuthorOrReadOnly)
+    permission_classes = (IsAuthorOrReadOnly,)
 
     def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        author = self.request.user
+        serializer.save(author=author)
 
     def perform_update(self, serializer):
-        serializer.save(author=self.request.user)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        author = self.request.user
+        serializer.save(author=author)
+    
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = (IsAuthorOrReadOnly,)
+
+    def get_queryset(self):
+        post = get_object_or_404(Post, id=self.kwargs.get('post_id'))
+        return post.comments.all()
+    
+    def perform_create(self, serializer):
+        post = get_object_or_404(Post, id=self.kwargs.get('post_id'))
+        author = self.request.user
+        serializer.save(author=author, post=post)
+    
+    def perform_update(self, serializer):
+        post = get_object_or_404(Post, id=self.kwargs.get('post_id'))
+        author = self.request.user
+        serializer.save(author=author, post=post)
