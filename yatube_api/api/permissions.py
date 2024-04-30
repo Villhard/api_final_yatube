6a55327feed73026aa2ee335e621a37cb34a2f1f
Victor Ryabov <victorwillhard@gmail.com>
2024-04-30 08:37:59 +0300

    Updated permissions class to use IsAuthenticatedOrReadOnly instead of BasePermission.

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -1,6 +1,6 @@
-from rest_framework.permissions import BasePermission, SAFE_METHODS
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, SAFE_METHODS
 
 
-class IsAuthorOrReadOnly(BasePermission):
+class IsAuthorOrReadOnly(IsAuthenticatedOrReadOnly):
     def has_object_permission(self, request, view, obj):
         return request.method in SAFE_METHODS or request.user == obj.author
