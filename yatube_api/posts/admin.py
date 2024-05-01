5ef31cfe07f9b63276cbe7b9c671be4e6a32c11a
Victor Ryabov <victorwillhard@gmail.com>
2024-05-01 15:37:12 +0300

    Add models to admin.py

--- yatube_api/posts/admin.py
+++ yatube_api/posts/admin.py
@@ -1,1 +1,26 @@
-# Register your models here.
+from posts.models import Comment, Follow, Group, Post
+from django.contrib import admin
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("pk", "text", "pub_date", "author")
+    actions = ("delete_selected",)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("pk", "text", "created", "author")
+    actions = ("delete_selected",)
+
+
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ("pk", "title", "slug", "description")
+    actions = ("delete_selected",)
+
+
+@admin.register(Follow)
+class FollowAdmin(admin.ModelAdmin):
+    list_display = ("pk", "user", "following")
+    actions = ("delete_selected",)
